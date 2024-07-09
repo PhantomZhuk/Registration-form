a0f5d6eb887c53a3e2a6e80a2beba254ce6ee2da
Maksym Zhuk <bitmaksum550@gmail.com>
2024-07-09 17:20:10 +0300

    add settings

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(`#loginInput`).focus(() => {
 });
 
 $(`#loginInput`).blur(() => {
-    if($(`#loginInput`).val().length < 1){
+    if ($(`#loginInput`).val().length < 1) {
         $(`.loginPlaceholder`).css({
             'margin-top': '20px',
             'padding': '0px',
@@ -33,7 +33,7 @@ $(`#emailInput`).focus(() => {
 });
 
 $(`#emailInput`).blur(() => {
-    if($(`#emailInput`).val().length < 1){
+    if ($(`#emailInput`).val().length < 1) {
         $(`.emailPlaceholder`).css({
             'margin-top': '80px',
             'padding': '0px',
@@ -55,7 +55,7 @@ $(`#passwordInput`).focus(() => {
 });
 
 $(`#passwordInput`).blur(() => {
-    if($(`#passwordInput`).val().length < 1){
+    if ($(`#passwordInput`).val().length < 1) {
         $(`.passwordPlaceholder`).css({
             'margin-top': '140px',
             'padding': '0px',
@@ -64,7 +64,7 @@ $(`#passwordInput`).blur(() => {
     }
 });
 
-$(`.viewPassword`).click(()=>{
+$(`.viewPassword`).click(() => {
     if ($(`.viewPassword`).hasClass('fa-eye-slash')) {
         $(`.viewPassword`).removeClass('fa-eye-slash').addClass('fa-eye');
         $(`#passwordInput`).attr('type', 'text');
@@ -76,41 +76,116 @@ $(`.viewPassword`).click(()=>{
 
 let checkColor = `#a7c957`;
 
-$(`#loginInput`).on(`input`, ()=>{
+$(`#loginInput`).on(`input`, () => {
     let login = $(`#loginInput`).val();
     $('.check1, .check2').css('color', '');
 
-    if (login.length >= 4){
+    if (login.length >= 4) {
         $(`.check1`).css(`color`, checkColor);
     }
 
-    if (!/\s/g.test(login)){
+    if (!/\s/g.test(login)) {
         $(`.check2`).css(`color`, checkColor);
     }
 });
 
-$(`#passwordInput`).on(`input`, ()=>{
+
+
+
+
+$(`#passwordInput`).on(`input`, () => {
     let password = $(`#passwordInput`).val();
 
     $(`.check4, .check5, .check6, .check7, .check8`).css(`color`, ``);
 
-    if (password.length >= 8 && password.length <= 12){
+    if (password.length >= 8 && password.length <= 12) {
         $(`.check4`).css(`color`, checkColor);
     }
 
-    if (/[!@#$%^&*()_+\-=\|?/><.,`~]/g.test(password)){
+    if (/[!@#$%^&*()_+\-=\|?/><.,`~]/g.test(password)) {
         $(`.check5`).css(`color`, checkColor);
     }
 
-    if (/[A-Z]/g.test(password)){
+    if (/[A-Z]/g.test(password)) {
         $(`.check6`).css(`color`, checkColor);
     }
 
-    if (/[a-z]/g.test(password)){
+    if (/[a-z]/g.test(password)) {
         $(`.check7`).css(`color`, checkColor);
     }
 
-    if (/[0-9]/g.test(password)){
+    if (/[0-9]/g.test(password)) {
         $(`.check8`).css(`color`, checkColor);
     }
+});
+
+$(`#settingsBtn`).click(() => {
+    $(`#settingsBtn`).css(`display`, `none`)
+
+    $(`.settingsContainer`).css({
+        'position': `absolute`,
+        'width': `300px`,
+        'height': `100px`,
+        'margin-left': `25px`,
+        'margin-bottom': `70px`,
+        'display': `flex`,
+        'flex-direction': `column`,
+        'justify-content': `space-between`,
+        'align-items': `center`,
+        'box-shadow': '0 3px 10px #333',
+        'padding': '10px',
+        'background-color': '#3a5a40'
+    });
+
+    $(`.settingsContainer`).append(`
+            <div class="settingsHeader">
+                <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div class="toggleBtn">
+                <div class="chosenBox"></div>
+                <div class="regular">Regular</div>
+                <div class="withoutRegular">Without regular</div>
+            </div>
+        `)
+});
+
+$(`.settingsContainer`).on(`click`, `.fa-xmark`, () => {
+    $(`#settingsBtn`).css({
+        'display': 'flex',
+        'padding': '8px 10px'
+    });
+    $(`.settingsContainer`).css({
+        'position': `relative`,
+        'width': `37px`,
+        'height': `32px`,
+        'border-radius': '5px',
+        'border': 'none',
+        'background-color': '#588157',
+        'color': '#fff',
+        'margin-left': `0px`,
+        'margin-bottom': `0px`,
+        'display': `flex`,
+        'justify-content': `center`,
+        'align-items': `center`,
+        'padding': '8px 10px',
+        'box-shadow': 'none',
+    });
+    $(`.settingsHeader`).remove();
+    $(`.toggleBtn`).remove();
+});
+
+$(`.settingsContainer`).on(`click`, `.regular`, () => {
+    $(`.chosenBox`).css({
+        'width': `100px`,
+        'height': `40px`,
+        'margin-left': `0px`,
+    });
+});
+
+$(`.settingsContainer`).on(`click`, `.withoutRegular`, () => {
+    $(`.chosenBox`).css({
+        'width': `120px`,
+        'height': `40px`,
+        'margin-left': `140px`,
+    });
 });
